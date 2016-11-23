5ca8bdfd9f3e7c40a35fae8a7c2b3e73428d2ed7
Allan Kenneth <hi@allankenneth.com>
2016-11-22 21:57:48 -0800

    Remove blog template in leui of using built-in Posts Page setting under Reading.

--- index.php
+++ index.php
@@ -5,15 +5,20 @@
 
 		<?php while ( have_posts() ) : the_post(); ?>
 
-		<div class="col-md-12">
+		<div class="col-md-6 offset-md-3">
 			<date><?php the_date() ?></date>
-			<h1><?php the_title() ?></h1>
-			<div class="content">
-				<?php the_content() ?>
+			<h1><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h1>
+<?php 
+    $mainbgtxtclass = get_theme_mod("hereditary_content_text");
+    $mainbgcolor = get_theme_mod("hereditary_contentbg");
+?>
+			<div class="<?php echo $mainbgtxtclass ?>" style="background: <?php echo $mainbgcolor ?>;">
+			<?php the_excerpt() ?>
 			</div>
+
 			<div class="editlink">
-					<?php edit_post_link( __( 'Edit' )); ?>
-				</div>
+			<?php edit_post_link( __( 'Edit' )); ?>
+			</div>
 		</div>
 
 		<?php endwhile; ?>
