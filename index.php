08a5b71f54fd60bc9fb2a144561039f438e66aff
Allan Kenneth <hi@allankenneth.com>
2016-12-12 22:59:57 -0800

    Blog index updates; removed cruft.

--- index.php
+++ index.php
@@ -1,23 +1,17 @@
-<?php get_header() ?>
+<?php $mainbgtxtclass = get_theme_mod("hereditary_content_text");
+$mainbgcolor = get_theme_mod("hereditary_contentbg");
+get_header() ?>
 <div class="container">
 	<div class="row">
-
 	<div class="col-md-5 offset-md-2">
 		<?php while ( have_posts() ) : the_post(); ?>
-
 		<div <?php post_class() ?>>
-			<date><?php the_date() ?></date>
 			<h1><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h1>
-<?php 
-    $mainbgtxtclass = get_theme_mod("hereditary_content_text");
-    $mainbgcolor = get_theme_mod("hereditary_contentbg");
-?>
+			<a class="featuredimage" href="<?php the_permalink() ?>">
+				<?php the_post_thumbnail('large') ?>
+			</a>
 			<div class="<?php echo $mainbgtxtclass ?>" style="background: <?php echo $mainbgcolor ?>;">
 			<?php the_excerpt() ?>
-			</div>
-
-			<div class="editlink">
-			<?php edit_post_link( __( 'Edit' )); ?>
 			</div>
 		</div>
 
