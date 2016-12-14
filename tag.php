632f387c6c9e9f396787475747d727e068a781a4
Allan Kenneth <hi@allankenneth.com>
2016-12-13 20:27:01 -0800

    Add tag template. Formatting is screwed.

---
+++ tag.php
@@ -0,0 +1,48 @@
+<?php get_header(); ?>
+	<div class="container">
+		<div class="row">
+
+		<?php if ( have_posts() ) : ?>
+			<header class="page-header">
+				<?php
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_description( '<div class="taxonomy-description">', '</div>' );
+				?>
+			</header><!-- .page-header -->
+<div class="col-md-5 offset-md-2">
+			<?php
+			// Start the Loop.
+			while ( have_posts() ) : the_post();
+
+		?>
+			<h1><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h1><?php
+			// End the loop.
+			endwhile;
+?>
+</div>
+<?php
+
+			// Previous/next page navigation.
+/*		the_posts_pagination( array(
+				'prev_text'          => __( 'Previous page', 'twentysixteen' ),
+				'next_text'          => __( 'Next page', 'twentysixteen' ),
+				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>',
+			) );
+*/
+		// If no content, include the "No posts found" template.
+		else :
+			get_template_part( 'template-parts/content', 'none' );
+
+		endif;
+		?>
+		
+		<div class="col-md-3">
+	<?php if ( is_active_sidebar( 'sidebar' ) ) : ?>
+		<?php dynamic_sidebar( 'sidebar' ); ?>
+	<?php endif; ?>
+
+		</div>
+		</div><!-- .row -->
+	</div><!-- .container -->
+
+<?php get_footer(); ?>
